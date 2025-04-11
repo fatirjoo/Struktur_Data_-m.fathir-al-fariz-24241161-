bc7d68233ad5640a45a4f96a3c9fff5a1d4568c9
fatirjoo <mfathiralfariz87@gmail.com>
2025-04-11 09:55:16 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,27 @@
+#perbandingan kemungkinan nilai =true/salah(bolean)
+#<, >, >=, <=, !=, not
+
+a = 24
+b = 53
+hasil = a < b
+print(hasil)
+
+a = 24
+b = 53
+hasil = a > b
+print(hasil)
+
+a = 24
+b = 53
+hasil = a <= b
+print(hasil)
+
+a = 24
+b = 53
+hasil = a != b
+print(hasil)
+
+a = 24
+b = 53
+hasil = a not b
+print(hasil)
