fc6013cb1b1cfde3d5fa00023249c69a8d32b564
fatirjoo <mfathiralfariz87@gmail.com>
2025-05-16 02:00:47 +0800

    meet5.py

---
+++ meet5.py
@@ -0,0 +1,19 @@
+#membuat program kalkulator sederhana
+#( + - x : )
+
+oprasi = input("anda ingin mengoprasikan apa (+ - x:):")
+bill = float(input("masukan bilangan pertama :"))
+bil2 = float(input("masukan bilangan kedua :"))
+
+if oprasi "+" :
+    final bill + bil2
+    print(final)
+elif oprasi == "
+    final bili - bil2
+    print(final)
+elif oprasi = "x" :
+    final bil1 * bil2
+    print(final)
+else
+    final bili / bil2
+    print(final)
